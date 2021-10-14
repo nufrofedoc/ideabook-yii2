21e9342857035636b44ba5fca136c369024eb234
Nebado <ruslan.nbd@gmail.com>
2021-10-14 19:03:02 +0300

    Change background for app page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,7 +20,11 @@
  @import url('https://fonts.googleapis.com/css2?family=Nothing+You+Could+Do&display=swap');
 
  body {
+     background: url('https://images.unsplash.com/photo-1551125735-900f662aeb07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80');
      background-color: #ececec;
-     font-family: 'Nothing You Could Do';
- } 
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      font-family: 'Nothing You Could Do';
+ }
 </style>
